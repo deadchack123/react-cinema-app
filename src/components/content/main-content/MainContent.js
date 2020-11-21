86580f122367c701258b6b05273ff6ec9535cb89
deadchack123 <deadchack221@gmail.com>
2020-11-21 16:05:47 +0400

    'addPagination'

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MainContent.scss'
 import SlideShow from '../slide-show/SlideShow'
+import Paginate from '../paginate/Paginate'
 
 const MainContent = () => {
   const images = [
@@ -14,12 +15,24 @@ const MainContent = () => {
       url: 'https://picsum.photos/1200/600'
     }
   ]
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const paginate = (type) => {
+    if (type === 'prev' && currentPage > 1) {
+      setCurrentPage((prev) => prev - 1)
+    } else {
+      setCurrentPage((prev) => prev + 1)
+    }
+  }
+
   return (
     <div className="main-content">
       <SlideShow images={images} auto={false} showArrows={true} />
       <div className="grid-movie-title">
         <div className="movieType">Now Playing</div>
-        <div className="paginate">Pagination</div>
+        <div className="paginate">
+          <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
+        </div>
       </div>
     </div>
   )
